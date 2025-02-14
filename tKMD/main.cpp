7c84077e3afd958c9b9b56bd821347594d80a410
Sergey Makarov <s.makarov@trovent.io>
2025-02-14 07:53:45 -0800

    provide implementation for IOCTL_LIST_MODULES and IOCTL_LIST_MODULES

--- tKMD/main.cpp
+++ tKMD/main.cpp
@@ -1,5 +1,6 @@
 #include <ntddk.h>
 #include <ntdef.h>
+#include <aux_klib.h>
 #include "ioctl.h"
 
 constexpr auto TAG = 'tKMD';
@@ -10,6 +11,8 @@ UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\tKMD");
 NTSTATUS DeviceControl(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp);
 NTSTATUS CreateClose(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp);
 void Unload(PDRIVER_OBJECT DriverObject);
+
+ULONG64 GetSystemRoutineAddress(PCWSTR routineName);
 
 extern "C" NTSTATUS DriverEntry(
 	_In_ PDRIVER_OBJECT DriverObject,
@@ -58,9 +61,149 @@ NTSTATUS DeviceControl(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp)
 
 	switch (stack->Parameters.DeviceIoControl.IoControlCode)
 	{
+	case IOCTL_LIST_MODULES:
+	{
+		DbgPrint("[*] IOCTL_LIST_MODULES\n");
+		ULONG szBuffer = 0;
+
+		if (NT_SUCCESS(status = AuxKlibInitialize()))
+		{
+			if (!NT_SUCCESS(status = AuxKlibQueryModuleInformation(&szBuffer, sizeof(AUX_MODULE_EXTENDED_INFO), nullptr)))
+			{
+				DbgPrint("[+] AuxKlibQueryModuleInformation failed: 0x%08X\n", status);
+				break;
+			}
+		}
+		else
+		{
+			DbgPrint("[+] AuxKlibInitialize failed: 0x%08X\n", status);
+			break;
+		}
+
+		auto modules = (PAUX_MODULE_EXTENDED_INFO)ExAllocatePool2(POOL_FLAG_PAGED, szBuffer, TAG);
+		if (modules == nullptr)
+		{
+			DbgPrint("[-] PAUX_MODULE_EXTENDED_INFO was null\n");
+			status = STATUS_INSUFFICIENT_RESOURCES;
+			break;
+		}
+
+		RtlZeroMemory(modules, szBuffer);
+
+		if (!NT_SUCCESS(status = AuxKlibQueryModuleInformation(&szBuffer, sizeof(AUX_MODULE_EXTENDED_INFO), modules)))
+		{
+			DbgPrint("[+] AuxKlibQueryModuleInformation failed: 0x%08X\n", status);
+			break;
+		}
+
+		auto numberOfModules = szBuffer / sizeof(AUX_MODULE_EXTENDED_INFO);
+
+		if (stack->Parameters.DeviceIoControl.OutputBufferLength < (sizeof(PMODULE_NAMES) * 256))
+		{
+			DbgPrint("[!] Caller's buffer is too small to hold MODULE_NAMES\n");
+			status = STATUS_BUFFER_TOO_SMALL;
+			break;
+		}
+
+		PMODULE_NAMES names = (PMODULE_NAMES)Irp->UserBuffer;
+
+		if (names == nullptr)
+		{
+			DbgPrint("[!] PMODULE_NAMES was null\n");
+			status = STATUS_INSUFFICIENT_RESOURCES;
+			break;
+		}
+
+		for (auto m = 0; m < numberOfModules; m++)
+		{
+			strcpy(names[m].Name, (char*)(modules[m].FullPathName));
+			length += sizeof(MODULE_NAMES);
+		}
+
+		ExFreePoolWithTag(modules, TAG);
+		break;
+	}
 	case IOCTL_CALLBACK_PROCESS:
 	{
 		DbgPrint("[*] IOCTL_CALLBACK_PROCESS");
+		ULONG64 psSetCreateProcessNotifyRoutine = GetSystemRoutineAddress(L"PsSetCreateProcessNotifyRoutine");
+		ULONG64 pspCreateProcessNotifyRoutineArray = psSetCreateProcessNotifyRoutine + PROCESS_NOTIFY_OFFSET;
+
+		ULONG szBuffer = 0;
+		
+		if (NT_SUCCESS(status = AuxKlibInitialize()))
+		{
+			if (!NT_SUCCESS(status = AuxKlibQueryModuleInformation(&szBuffer, sizeof(AUX_MODULE_EXTENDED_INFO), nullptr)))
+			{
+				DbgPrint("[+] AuxKlibQueryModuleInformation failed: 0x%08X\n", status);
+				break;
+			}
+		}
+		else
+		{
+			DbgPrint("[+] AuxKlibInitialize failed: 0x%08X\n", status);
+			break;
+		}
+
+		auto modules = (PAUX_MODULE_EXTENDED_INFO)ExAllocatePool2(POOL_FLAG_PAGED, szBuffer, TAG);
+		if (modules == nullptr)
+		{
+			DbgPrint("[-] PAUX_MODULE_EXTENDED_INFO was null\n");
+			status = STATUS_INSUFFICIENT_RESOURCES;
+			break;
+		}
+
+		RtlZeroMemory(modules, szBuffer);
+
+		if (!NT_SUCCESS(status = AuxKlibQueryModuleInformation(&szBuffer, sizeof(AUX_MODULE_EXTENDED_INFO), modules)))
+		{
+			DbgPrint("[+] AuxKlibQueryModuleInformation failed: 0x%08X\n", status);
+			break;
+		}
+
+		auto numberOfModules = szBuffer / sizeof(AUX_MODULE_EXTENDED_INFO);
+		ULONG64 arrayPointer = pspCreateProcessNotifyRoutineArray;
+
+		if (stack->Parameters.DeviceIoControl.OutputBufferLength < (sizeof(CALLBACK_PROCESS) * 256))
+		{
+			DbgPrint("[!] Buffer too small to hold CALLBACK_PROCESS\n");
+			status = STATUS_BUFFER_TOO_SMALL;
+			break;
+		}
+
+		PCALLBACK_PROCESS callbackInfo = (PCALLBACK_PROCESS)Irp->UserBuffer;
+
+		if (callbackInfo == nullptr)
+		{
+			DbgPrint("[!] PCALLBACK_PROCESS was null.\n");
+			status = STATUS_INSUFFICIENT_RESOURCES;
+			break;
+		}
+
+		for (auto i = 0; i < 64; i++)
+		{
+			ULONG64 callbackAddress = *(PULONG64)(arrayPointer);
+			if (callbackAddress > 0)
+			{
+				ULONG64 rawPointer = *(PULONG64)(callbackAddress & 0xfffffffffffffff8);
+				for (auto m = 0; m < numberOfModules; m++)
+				{
+					auto startAddress = (ULONG64)modules[m].BasicInfo.ImageBase;
+					auto endAddress = (ULONG64)(startAddress + modules[m].ImageSize);
+
+					if (rawPointer > startAddress && rawPointer < endAddress)
+					{
+						strcpy(callbackInfo[i].Module, (char*)modules[m].FullPathName);
+						callbackInfo[i].Address = rawPointer;
+						break;
+					}
+				}
+				length += sizeof(CALLBACK_PROCESS);
+			}
+			arrayPointer += 8;
+		}
+
+		ExFreePoolWithTag(modules, TAG);
 		break;
 	}
 	case IOCTL_CALLBACK_THREAD:
@@ -85,7 +228,7 @@ NTSTATUS DeviceControl(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp)
 	Irp->IoStatus.Information = length;
 	IoCompleteRequest(Irp, IO_NO_INCREMENT);
 		
-	return STATUS_SUCCESS;
+	return status;
 }
 
 void Unload(PDRIVER_OBJECT DriverObject)
@@ -103,4 +246,12 @@ NTSTATUS CreateClose(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp)
 	Irp->IoStatus.Information = 0;
 	IoCompleteRequest(Irp, IO_NO_INCREMENT);
 	return STATUS_SUCCESS;
+}
+
+ULONG64 GetSystemRoutineAddress(PCWSTR routineName)
+{
+	UNICODE_STRING functionName;
+	RtlInitUnicodeString(&functionName, routineName);
+
+	return (ULONG64)MmGetSystemRoutineAddress(&functionName);
 }
--- tKMDc/main.cpp
+++ tKMDc/main.cpp
@@ -1,9 +1,18 @@
 ﻿#include <Windows.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "../tKMD/ioctl.h"
 
 int main(int argc, char * argv[])
 {
+	if (argc < 2)
+	{
+		printf("Usage: .exe <int:toggle>\n\t1: LIST KERNEL MODULES\n\t2: LIST PROCESSNOTIFY CALLBACKS\n\t3: LIST THREADNOTIFY CALLBACKS\n\t4: LIST IMAGENOTIFY CALLBACKS\n\t5: DISABLE PROCESSNOTIFY CALLBACK\n");
+		return 1;
+	}
+
+	int toggle = atoi(argv[1]);
+
 	HANDLE hDriver;
 	BOOL success; 
 
@@ -11,19 +20,47 @@ int main(int argc, char * argv[])
 
 	if (hDriver == INVALID_HANDLE_VALUE)
 	{
-		printf("[-] failed to open a handle to the driver: %d", GetLastError());
+		printf("[-] failed to open a handle to the driver: %d\n", GetLastError());
 		return 1;
 	}
 
-	success = DeviceIoControl(hDriver, IOCTL_CALLBACK_PROCESS, nullptr, 0, nullptr, 0, nullptr, nullptr);
+	switch (toggle)
+	{
+	case 1: 
+	{
+		MODULE_NAMES modules[256];
+		RtlZeroMemory(modules, sizeof(modules));
 
-	if (success)
-	{
-		printf("[+] processing IOCTL_CALLBACK_PROCESS\n");
+		if (success = DeviceIoControl(hDriver, IOCTL_LIST_MODULES, nullptr, 0, &modules, sizeof(modules), nullptr, nullptr))
+		{
+			for (auto i = 0; i < 256; i++)
+			{
+				if (strlen(modules[i].Name) > 0)
+				{
+					printf("\t[%d] %s\n", i, modules[i].Name);
+				}
+			}
+		}
+		break;
 	}
-	else 
+	case 2:
 	{
-		printf("[+] failed processing IOCTL_CALLBACK_PROCESS: %d\n", GetLastError());
+		CALLBACK_PROCESS callbacks[256];
+		RtlZeroMemory(callbacks, sizeof(callbacks));
+
+		if (success = DeviceIoControl(hDriver, IOCTL_CALLBACK_PROCESS, nullptr, 0, &callbacks, sizeof(callbacks), nullptr, nullptr))
+		{
+			for (auto i = 0; i < 64; i++)
+			{
+				if (callbacks[i].Address == 0)
+				{
+					continue;
+				}
+				printf("\t[%d] 0x%llx -> %s\n", i, callbacks[i].Address, callbacks[i].Module);
+			}
+		}
+		break;
+	}
 	}
 
 	CloseHandle(hDriver);
